d94c28e6cc22aa7d1032e57e398cc7a5fb84d083
kimmabrandt <kimmabrandt@gmail.com>
2017-03-13 20:17:20 -0700

    routes/services/controllers/dependency injection - practice

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// create a global module for the app where we can create routes, models, filters and directives
+// App variable is accessible thruout application
+// ngResource is an addtl module that defines functionality for creating resources (models) in angular
+var App = angular.module('angular-app', ['ngResource']);
